933e4509cdfa7870037783ae579720174c4e410a
Sungjin Lee <junion.sjlee@gmail.com>
2014-06-01 22:15:42 -0400

    working on rules for letsgo

--- cornerstone/controller/rule_engine/python_sml_test.py
+++ cornerstone/controller/rule_engine/python_sml_test.py
@@ -1,8 +1,21 @@
-import sys
-#sys.path.append('.')
-import soar.Python_sml_ClientInterface as sml
 
-k = sml.Kernel.CreateKernelInNewThread()
-a = k.CreateAgent('soar')
-print a.ExecuteCommandLine('echo hello world!!!')
+import rule_engine_api as reapi
+
+kernel = reapi.create_kernel()
+agent = reapi.create_agent(kernel, "agent")
+reapi.register_print_callback(kernel, agent,
+                              reapi.callback_print_message, None)
+input_link_wme = agent.GetInputLink()
+
+# load domain rules
+domain_rule_source = '../../../model/rules/Letsgo.soar'
+print agent.LoadProductions(domain_rule_source)
+print agent.ExecuteCommandLine('print')
+
+print agent.RunSelfTilOutput('run')
+
+kernel.DestroyAgent(agent)
+kernel.Shutdown()
+del kernel
+
 

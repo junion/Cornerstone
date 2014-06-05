539c7d45a661b160a861e9f2495afd4c98d7dbf2
Sungjin Lee <junion.sjlee@gmail.com>
2014-06-04 22:51:58 -0400

    working on letsgo rules

--- cornerstone/controller/rule_engine/python_sml_test.py
+++ cornerstone/controller/rule_engine/python_sml_test.py
@@ -1,4 +1,5 @@
 
+from pprint import pprint
 import rule_engine_api as reapi
 
 def test1():
@@ -12,15 +13,47 @@ def test1():
     domain_rule_source = '../../../model/rules/Letsgo.soar'
     print agent.LoadProductions(domain_rule_source)
     print agent.ExecuteCommandLine('print')
- 
+    agent.ExecuteCommandLine('set-stop-phase -Ao')
     # begin-session   
-    print agent.RunSelfTilOutput('run')
+    print agent.RunSelf(3)
+    print '-----'
+    cmds = {}
+    for i in range(agent.GetNumberCommands()):
+        cmd = agent.GetCommand(i)
+        cmd_name = cmd.GetCommandName()
+        params = {}
+        for j in range(cmd.GetNumberChildren()):
+            param = cmd.GetChild(j)
+            param_name = param.GetAttribute()
+            params[param_name] = cmd.GetParameterValue(param_name)
+        cmds[cmd_name] = params
+        cmd.AddStatusComplete()
+    agent.ClearOutputLinkChanges()
+    pprint(cmds)
+    print '#####'    
     events = agent.CreateIdWME(input_link_wme, 'events')
     begin = agent.CreateStringWME(events, 'begin-session', 'nil')
-    print agent.RunSelfTilOutput('run')
+    print agent.RunSelf(1)
+    cmds = {}
+    for i in range(agent.GetNumberCommands()):
+        cmd = agent.GetCommand(i)
+        cmd_name = cmd.GetCommandName()
+        params = {}
+        for j in range(cmd.GetNumberChildren()):
+            param = cmd.GetChild(j)
+            param_name = param.GetAttribute()
+            print param_name
+            print param.GetValueAsString()
+            params[param_name] = param.GetValueAsString() #cmd.GetParameterValue(param_name)
+        cmds[cmd_name] = params
+        cmd.AddStatusComplete()
+    agent.ClearOutputLinkChanges()
+    pprint(cmds)
+
+
     
     kernel.DestroyAgent(agent)
     kernel.Shutdown()
     del kernel
 
-
+test1()

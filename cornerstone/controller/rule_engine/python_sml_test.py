4dbd7a3eab449f63ceac3cfe3fa6bd4fb1f9986e
Sungjin Lee <junion.sjlee@gmail.com>
2014-06-05 14:12:57 -0400

    working on letsgo rules

--- cornerstone/controller/rule_engine/python_sml_test.py
+++ cornerstone/controller/rule_engine/python_sml_test.py
@@ -2,6 +2,40 @@
 from pprint import pprint
 import rule_engine_api as reapi
 
+def get_cmds(agent):
+    cmds = {}
+    output_link_wme = agent.GetOutputLink()
+    if output_link_wme:
+        for i in range(output_link_wme.GetNumberChildren()):
+            cmd = output_link_wme.GetChild(i).ConvertToIdentifier()
+            cmd_name = cmd.GetAttribute()
+#            print cmd_name
+            params = {}
+            for j in range(cmd.GetNumberChildren()):
+                param = cmd.GetChild(j)
+                param_name = param.GetAttribute()
+#                print param_name
+#                print param.GetValueAsString()
+                params[param_name] = param.GetValueAsString() 
+            if 'status' not in params:
+                cmds[cmd_name] = params
+                agent.CreateStringWME(cmd, 'status', 'complete')
+                agent.Commit()
+    pprint(cmds)
+    return cmds
+    
+
+def get_output(agent):
+    while True:
+#        print agent.ExecuteCommandLine('print -d 4 i1')
+        print agent.RunSelf(1)
+#        print agent.ExecuteCommandLine('print -d 4 i1')
+        cmds = get_cmds(agent)
+        if cmds:
+            break
+    return cmds
+
+
 def test1():
     kernel = reapi.create_kernel()
     agent = reapi.create_agent(kernel, "agent")
@@ -14,43 +48,46 @@ def test1():
     print agent.LoadProductions(domain_rule_source)
     print agent.ExecuteCommandLine('print')
     agent.ExecuteCommandLine('set-stop-phase -Ao')
-    # begin-session   
-    print agent.RunSelf(3)
-    print '-----'
-    cmds = {}
-    for i in range(agent.GetNumberCommands()):
-        cmd = agent.GetCommand(i)
-        cmd_name = cmd.GetCommandName()
-        params = {}
-        for j in range(cmd.GetNumberChildren()):
-            param = cmd.GetChild(j)
-            param_name = param.GetAttribute()
-            params[param_name] = cmd.GetParameterValue(param_name)
-        cmds[cmd_name] = params
-        cmd.AddStatusComplete()
-    agent.ClearOutputLinkChanges()
-    pprint(cmds)
-    print '#####'    
-    events = agent.CreateIdWME(input_link_wme, 'events')
-    begin = agent.CreateStringWME(events, 'begin-session', 'nil')
-    print agent.RunSelf(1)
-    cmds = {}
-    for i in range(agent.GetNumberCommands()):
-        cmd = agent.GetCommand(i)
-        cmd_name = cmd.GetCommandName()
-        params = {}
-        for j in range(cmd.GetNumberChildren()):
-            param = cmd.GetChild(j)
-            param_name = param.GetAttribute()
-            print param_name
-            print param.GetValueAsString()
-            params[param_name] = param.GetValueAsString() #cmd.GetParameterValue(param_name)
-        cmds[cmd_name] = params
-        cmd.AddStatusComplete()
-    agent.ClearOutputLinkChanges()
-    pprint(cmds)
 
+    # begin-session
+    while True:   
+        cmds = get_output(agent)
+        if 'wait' in cmds:
+            events = agent.CreateIdWME(input_link_wme, 'events')
+            begin = agent.CreateStringWME(events, 'begin-session', 'nil')
+            agent.Commit()
+            break
+ 
+    cmds = get_output(agent)
+    agent.DestroyWME(events)
+    agent.Commit()
 
+    while True:
+        cmds = get_output(agent)
+        if 'wait' in cmds:
+            print agent.ExecuteCommandLine('print -d 4 s1')
+            input_link_wme = agent.GetInputLink()
+            events = agent.CreateIdWME(input_link_wme, 'events')
+            cu = agent.CreateIdWME(events, 'concept-update')
+            agent.CreateStringWME(cu, 'name', 'from')
+            hyp1 = agent.CreateIdWME(cu, 'top-hyp')
+            agent.CreateStringWME(hyp1, 'value', 'cmu')
+            agent.CreateFloatWME(hyp1, 'score', 0.7)
+            hyp2 = agent.CreateIdWME(cu, 'second-hyp')
+            agent.CreateStringWME(hyp2, 'value', 'none')
+            agent.CreateFloatWME(hyp2, 'score', 0.0)
+            hyp3 = agent.CreateIdWME(cu, 'rest-hyp')
+            agent.CreateStringWME(hyp3, 'value', 'none')
+            agent.CreateFloatWME(hyp3, 'score', 0.0)
+            agent.Commit()
+            break
+
+    print agent.ExecuteCommandLine('print -d 4 i2')
+    cmds = get_output(agent)
+#    agent.DestroyWME(events)
+#    agent.Commit()
+#    
+    print agent.ExecuteCommandLine('print -d 4 s1')
     
     kernel.DestroyAgent(agent)
     kernel.Shutdown()

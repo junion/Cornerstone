5b01e16227ffa4dbb9b07c71254ff0b4ee13ecb4
Sungjin Lee <junion@Sungjins-MacBook-Air.local>
2014-08-02 14:13:34 +0900

    working...

---
+++ core/state_tracker/simple_rule_state_tracker.py
@@ -0,0 +1,70 @@
+'''
+Created on Jul 1, 2014
+
+@author: Sungjin Lee
+'''
+
+import logging
+
+from state_tracker import StateTracker
+from core.datatypes.concept_belief_state import ConceptBeliefState
+
+
+MODULE_ID = 'SimpleRuleStateTracker'
+
+
+class SimpleRuleStateTracker(StateTracker):
+    def __init__(self):
+        StateTracker.__init__(self)
+
+    def update_concept_belief(self, state, concept, inform_obs, 
+                              affirm_obs, negate_obs):
+        if concept not in state.concepts:
+            cb_state = ConceptBeliefState()
+        else:
+            cb_state = state.concepts[concept]
+        for item, score in inform_obs:
+            cb_state[item] = cb_state[item] + score
+        for item, score in affirm_obs:
+            cb_state[item] += score
+        for item, score in negate_obs:
+            cb_state[item] -= min(cb_state[item], score)
+        state.concepts[concept] = cb_state
+        self.app_logger.info('\nConcept %s update:\n%s' % 
+                             (concept, str(state.concepts[concept])))
+                
+
+if __name__ == '__main__':
+    from core.state_tracker.state import State
+    from core.datatypes.speech_event import (
+         SpeechEvent, SpeechNbest, SpeechTurn, SpeechAct)
+    from core.datatypes.event_list import EventList
+    
+    # create state
+    state = State()
+    print state
+    # speech out event
+    nbest = SpeechNbest()
+    t = SpeechTurn()
+    t.append(SpeechAct('expl-conf', [('route', '54c')]))
+    nbest.append(t, 1.0)
+    out_event = SpeechEvent(nbest)
+    # last speech out event
+    state.last_speech_out_event = out_event
+    # speech in event
+    nbest = SpeechNbest()
+    t = SpeechTurn()
+    t.append(SpeechAct('inform', [('from', 'cmu'), ('to', 'port')]))
+    t.append(SpeechAct('affirm', []))
+    nbest.append(t, 0.71)
+    t = SpeechTurn()
+    t.append(SpeechAct('inform', [('from', 'cmu'), ('to', 'airport')]))
+    t.append(SpeechAct('negate', []))
+    nbest.append(t, 0.2)
+    in_event = SpeechEvent(nbest)
+    in_events = EventList()
+    in_events.add_event('speech', in_event)
+    # state tracker
+    sr_tracker = SimpleRuleStateTracker()
+    sr_tracker.update_belief_state(state, in_events)
+    print state
--- core/state_tracker/state_tracker.py
+++ core/state_tracker/state_tracker.py
@@ -7,7 +7,6 @@ Created on Jul 1, 2014
 import logging
 
 from config.global_config import get_config
-from core.datatypes.concept_belief_state import ConceptBeliefState
 
 
 MODULE_ID = 'StateTracker'
@@ -40,21 +39,6 @@ class StateTracker(object):
                 state.execute_result = event.args
         
     def update_belief_state(self, state, in_events):
-        raise NotImplementedError
-    
-    def update_last_out_events(self, state, out_events):
-        # LIMIT: currently assume that 
-        # the system makes only a speech out event
-        speech_events = out_events.get_events('speech')
-        if speech_events:
-            state.last_speech_out_event = speech_events[0]
-
-
-class SimpleRuleStateTracker(StateTracker):
-    def __init__(self):
-        StateTracker.__init__(self)
-
-    def update_belief_state(self, state, in_events):
         for event in in_events.get_events('speech'):
             in_nbest = event.speech_nbest
             in_concepts = in_nbest.get_concepts()
@@ -82,64 +66,10 @@ class SimpleRuleStateTracker(StateTracker):
                                       for c_act in confirm_acts]
                 self.update_concept_belief(state, concept, inform_obs,
                                            affirm_obs, negate_obs)
-
-    def update_concept_belief(self, state, concept, inform_obs, 
-                              affirm_obs, negate_obs):
-        if concept not in state.concepts:
-            cb_state = ConceptBeliefState()
-#             state.concepts[concept] = cb_state
-        else:
-            cb_state = state.concepts[concept]
-        for item, score in inform_obs:
-            cb_state[item] = cb_state[item] + score
-        for item, score in affirm_obs:
-            cb_state[item] += score
-        for item, score in negate_obs:
-            cb_state[item] -= min(cb_state[item], score)
-        state.concepts[concept] = cb_state
-        self.app_logger.info('\nConcept %s update:\n%s' % 
-                             (concept, str(state.concepts[concept])))
-                
-
-if __name__ == '__main__':
-    from core.state_tracker.state import State
-    from core.datatypes.speech_event import (
-         SpeechEvent, SpeechNbest, SpeechTurn, SpeechAct)
-    from core.datatypes.event_list import EventList
     
-    # create state
-    state = State()
-    print state
-    # speech out event
-    nbest = SpeechNbest()
-    t = SpeechTurn()
-    t.append(SpeechAct('expl-conf', [('route', '54c')]))
-    nbest.append(t, 1.0)
-#     print nbest
-    out_event = SpeechEvent(nbest)
-    # last speech out event
-    state.last_speech_out_event = out_event
-    # speech in event
-    nbest = SpeechNbest()
-    t = SpeechTurn()
-    t.append(SpeechAct('inform', [('from', 'cmu'), ('to', 'port')]))
-    t.append(SpeechAct('affirm', []))
-    nbest.append(t, 0.71)
-    t = SpeechTurn()
-    t.append(SpeechAct('inform', [('from', 'cmu'), ('to', 'airport')]))
-    t.append(SpeechAct('negate', []))
-    nbest.append(t, 0.2)
-#     print nbest
-#     pprint(nbest.marginal('inform', 'from'))
-#     pprint(nbest.marginal('inform', 'to'))
-#     pprint(nbest.marginal('inform', 'time'))
-#     pprint(nbest.marginal('affirm'))
-#     pprint(nbest.marginal('negate'))
-#     print nbest.get_concepts()
-    in_event = SpeechEvent(nbest)
-    in_events = EventList()
-    in_events.add_event('speech', in_event)
-    # state tracker
-    sr_tracker = SimpleRuleStateTracker()
-    sr_tracker.update_belief_state(state, in_events)
-    print state
+    def update_last_out_events(self, state, out_events):
+        # LIMIT: currently assume that 
+        # the system makes only a speech out event
+        speech_events = out_events.get_events('speech')
+        if speech_events:
+            state.last_speech_out_event = speech_events[0]
